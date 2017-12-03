995f666c682a67c279b043ad0d8706d447b092f7
Lasse Legaard <legaard.lasse@gmail.com>
2017-12-03 16:55:56 +0100

    Added student search input to sidebar and student toggles

--- src/app/shell/sidebar/sidebar.component.ts
+++ src/app/shell/sidebar/sidebar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as StudentToggleActions from '../../actions/student-toggle-actions';
+import { Student } from '../../shared/models/student.model';
+import { AppState } from '../../reducers/reducers';
+import { StudentToggle } from '../../shared/models/student-toggle.model';
 
 @Component({
   selector: 'sg-sidebar',
@@ -6,11 +12,32 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  studentToggles: StudentToggle[];
+  student: Student;
+
   @Input() isVisible: boolean;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store
+      .select(state => state.studentToggles.toggles)
+      .subscribe(toggles => this.studentToggles = toggles);
   }
 
+  onStudentFound(student: Student) {
+    this.student = student;
+  }
+
+  addStudent(student: Student) {
+    this.store.dispatch(new StudentToggleActions.AddStudent(student));
+  }
+
+  removeStudent(id: string) {
+    this.store.dispatch(new StudentToggleActions.RemoveStudent(id));
+  }
+
+  toggleStudent(id: string) {
+    this.store.dispatch(new StudentToggleActions.ToggleStudent(id));
+  }
 }
